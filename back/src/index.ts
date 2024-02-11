06f4e843e554e6024f508d8ee8b68920f320e185
salino3 <programacionflavio@gmail.com>
2024-02-11 16:15:28 +0100

    add firsts objects

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import axios from 'axios';
+import { send } from "process";
 
 
 
@@ -12,11 +14,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get('/api/', (req, res) => {
+app.get('/api/', (req: Request, res: Response) => { 
     res.send("Hello world!")
 });
 
-app.listen(() => {
+app.get('/api/products', async (req: Request, res: Response) => {
+
+    try {
+        
+     const products = await axios.get("http://localhost:3000/products");
+    
+     if(!products.data || products.data.lenght < 1){
+        return res.send({message: "The list is empty"});
+     };
+     
+     res.send(products.data);
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(401);
+    };
+})
+
+app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
 });
 
